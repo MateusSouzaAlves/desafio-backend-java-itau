015ad902a986d2b8e629b208a8986683c6d996e0
solutionmateus <solution.mateus@gmail.com>
2025-07-21 11:49:12 -0300

    feat: estrutura inicial do projeto BackendItau com classes principais e configuração

---
+++ src/main/java/com/desafio/itau/backend/model/Estatisticas.java
@@ -0,0 +1,61 @@
+package com.desafio.itau.backend.model;
+
+public class Estatisticas {
+    private long count;
+    private double sum;
+    private double avg;
+    private double min;
+    private double max;
+
+    public Estatisticas() {
+
+    }
+
+    public Estatisticas(Long count, double sum, double avg, double min, double max) {
+        this.count = count;
+        this.sum = sum;
+        this.avg = avg;
+        this.min = min;
+        this.max = max;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    public double getMin() {
+        return min;
+    }
+
+    public void setMin(double min) {
+        this.min = min;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
+
+    public double getAvg() {
+        return avg;
+    }
+
+    public void setAvg(double avg) {
+        this.avg = avg;
+    }
+
+    public double getMax() {
+        return max;
+    }
+
+    public void setMax(double max) {
+        this.max = max;
+    }
+}
