4a8d54a686df6670c7078af48f2547ec386757c3
solutionmateus <solution.mateus@gmail.com>
2025-07-21 12:24:05 -0300

    feat: endpoint de deletar transações

--- src/main/java/com/desafio/itau/backend/controller/TransacaoController.java
+++ src/main/java/com/desafio/itau/backend/controller/TransacaoController.java
@@ -4,10 +4,7 @@ package com.desafio.itau.backend.controller;
 import com.desafio.itau.backend.model.Transacao;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.OffsetDateTime;
 import java.util.List;
@@ -39,4 +36,11 @@ public class TransacaoController {
         transacoes.add(transacao);
         return true;
     }
+
+    @DeleteMapping("/transacao")
+    public ResponseEntity<Void> deletarTransacoes(){
+        transacoes.clear();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
