2c4b8984d66a259e4b84b660de48f9bfdecc4434
solutionmateus <solution.mateus@gmail.com>
2025-07-21 12:17:55 -0300

    feat: endpoint de postar transações

---
+++ src/main/java/com/desafio/itau/backend/controller/TransacaoController.java
@@ -0,0 +1,42 @@
+package com.desafio.itau.backend.controller;
+
+
+import com.desafio.itau.backend.model.Transacao;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@RestController
+@RequestMapping
+public class TransacaoController {
+
+    private final List<Transacao> transacoes = new CopyOnWriteArrayList<>();
+
+    @PostMapping("/transacao")
+    public ResponseEntity<Void> criarTransacao(@RequestBody Transacao transacao){
+        boolean adicionada = adicionarTransacao(transacao);
+
+        if (adicionada == true){
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    private boolean adicionarTransacao(Transacao transacao) {
+
+        if (transacao.getValor() < 0 || transacao.getDataHora().isAfter(OffsetDateTime.now())) {
+            return false;
+        }
+
+        transacoes.add(transacao);
+        return true;
+    }
+}
