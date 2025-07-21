de8d84d116e058b6ed6e57e42afeea18e9f3f83f
solutionmateus <solution.mateus@gmail.com>
2025-07-21 13:03:48 -0300

    feat: endpoint de buscar transações nos últimos 60 segundos

--- src/main/java/com/desafio/itau/backend/controller/TransacaoController.java
+++ src/main/java/com/desafio/itau/backend/controller/TransacaoController.java
@@ -1,12 +1,14 @@
 package com.desafio.itau.backend.controller;
 
 
+import com.desafio.itau.backend.model.Estatisticas;
 import com.desafio.itau.backend.model.Transacao;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.OffsetDateTime;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -41,6 +43,25 @@ public class TransacaoController {
     public ResponseEntity<Void> deletarTransacoes(){
         transacoes.clear();
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/estatistica")
+    public ResponseEntity<Estatisticas> obterEstatisticas(){
+        OffsetDateTime agora = OffsetDateTime.now();
+
+        DoubleSummaryStatistics status60s = transacoes.stream().filter( t -> t.getDataHora().isAfter(agora.minusSeconds(60)))
+                .mapToDouble(Transacao::getValor)
+                .summaryStatistics();
+
+        Estatisticas estatisticas = new Estatisticas(
+                status60s.getCount(),
+                status60s.getSum(),
+                status60s.getAverage(),
+                status60s.getCount() == 0 ? 0 : status60s.getMin(),
+                status60s.getCount() == 0 ? 0 : status60s.getMax()
+        );
+
+        return ResponseEntity.ok(estatisticas);
     }
 
 }
